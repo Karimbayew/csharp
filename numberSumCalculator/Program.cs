0a399c0a422305c5b76caa212c1cb5752ba954c3
Shahzad <karimbayewshahzad@gmail.com>
2025-10-16 13:56:43 +0300

    Задание 2: Сумма чисел

---
+++ numberSumCalculator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double sum = 0;
+        
+        Console.WriteLine("Вводите числа по одному (для завершения введите 0):");
+        
+        while (true)
+        {
+            Console.Write("Введите число: ");
+            string input = Console.ReadLine()!;
+            
+            if (double.TryParse(input, out double number))
+            {
+                if (number == 0)
+                {
+                    Console.WriteLine("Программа завершена.");
+                    break;
+                }
+                
+                sum += number;
+                Console.WriteLine($"Текущая сумма: {sum}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: введите корректное число!");
+            }
+        }
+        
+        Console.WriteLine($"Итоговая сумма: {sum}");
+        Console.ReadKey();
+    }
+}
