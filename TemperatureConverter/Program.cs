11430ea02f5a9fb339fc6d24d96ba51f5f3aa404
Shahzad <karimbayewshahzad@gmail.com>
2025-10-16 13:55:28 +0300

    Задание 1: Конвертер температур

---
+++ TemperatureConverter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите температуру в градусах Цельсия: ");
+        string? input = Console.ReadLine();
+        
+        if (input != null && double.TryParse(input, out double celsius))
+        {
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            Console.WriteLine($"{celsius}°C = {fahrenheit}°F");
+        }
+        else
+        {
+            Console.WriteLine("Ошибка: введите корректное число!");
+        }
+    
+        Console.ReadKey();
+    }
+}
